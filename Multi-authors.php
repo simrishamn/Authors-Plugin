e711ea39c4ca5eba2b11947e01bc90e05ce95590
Magdalena Korpela <magdalena.osbakk@gmail.com>
2018-08-13 14:47:37 +0200

    Start up project

---
+++ Multi-authors.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Multi authors plugin
+ * Description: A simple plugin for adding multiple authors to a post
+ * Version: 0.0
+ * Author: Magdalena Korpela
+ * Author URI: https://github.com/magosb
+ * License: MIT
+ * 
+ */
+defined( 'ABSPATH' ) or die( 'Access denied' );
+
+add_post_meta($post_id, 'test', 'test');
